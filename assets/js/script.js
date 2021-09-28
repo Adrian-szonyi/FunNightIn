01a13e49e06ca419f3b6a54272ccbcb47bd5b3e5
unknown <aszonyi49@gmail.com>
2021-09-29 08:25:48 +1000

    Randomly generates a movie title and description

--- assets/js/script.js
+++ assets/js/script.js
@@ -11,35 +11,52 @@ $(function () {
   $("#rating").selectmenu();
 });
 
-var newImage = document.querySelector("#Newimage")
-var MovieImage = document.querySelector("#Movieposter")
+// Generate random movie and meal button
+var generateBtn = document.querySelector("#generate-button");
+// Generate with filters movie and meal button
+
+// var filterBtn = document.querySelector("#filter-button");
+// //Generated movie content
+var movieTitle = document.querySelector("#movie-title");
+var overviewDiv = document.querySelector("#overview");
+// //Generated recipe content
+// var recipeTitle = document.querySelector("recipe-title");
+// var ingredientEl = document.querySelector("ingredients");
+// var methodDiv = document.querySelector("method");
+
+var filterBtn = document.querySelector("filter-button");
+
+var newImage = document.querySelector("#Newimage");
+var MovieImage = document.querySelector("#MoviePoster");
+
+generateBtn.addEventListener("click", getMovies);
 async function getMovies() {
-  var randommovie = Math.floor(Math.random() * 1000);
+  var randompage = Math.floor(Math.random() * 20);
+  var randommovie = Math.floor(Math.random() * 19);
   var movies = await fetch(
-    "https://api.themoviedb.org/3/discover/movie?sort_by=popularity.desc&api_key=7113d8f1b2a86dd4b4fe2e64488fe988"
+    "https://api.themoviedb.org/3/discover/movie?sort_by=popularity.desc&vote_count.gte10000&original_language=en-US&page=" + randompage + "&api_key=7113d8f1b2a86dd4b4fe2e64488fe988"
   ).then((response) => response.json());
-  console.log(movies);
-  console.log(randommovie);
-  var movieposter = "https://image.tmdb.org/t/p/w500/" + movies.results[0].poster_path;
-console.log(movies.results[0].poster_path)
-console.log(movieposter)
-MovieImage.src = movieposter;
+  movies.results.length = 200;
+  var movieposter =
+    "https://image.tmdb.org/t/p/w500/" + movies.results[randommovie].poster_path;
+  MovieImage.src = movieposter;
 
-//   console.log(movieposter)
-  }
+  console.log(movies)
+  console.log(randommovie)
+  console.log(movies.results[100])
+  console.log(movies.results[randommovie].original_title)
+  movieTitle.textContent = movies.results[randommovie].original_title
+  overviewDiv.textContent = movies.results[randommovie].overview
 
-  getMovies() 
+}
 
-  //Array.map
-=======
 
 generateBtn.addEventListener("click", getRandomMeal);
 
-function getRandomMeal () {
+function getRandomMeal() {
   fetch(`https://www.themealdb.com/api/json/v1/1/random.php`)
-    .then(response => response.json())
-    .then(data => {
-      console.log(data)
-    })
+    .then((response) => response.json())
+    .then((data) => {
+      console.log(data);
+    });
 }
-
