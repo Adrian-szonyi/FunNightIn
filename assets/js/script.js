da0f522bc64d0b21517c6903f19271c2b50124ae
lilys0 <lsouryaphanh@gmail.com>
2021-09-28 20:40:36 +1000

    button update

--- assets/js/script.js
+++ assets/js/script.js
@@ -5,3 +5,8 @@ $(function () {
 
   $("#rating").selectmenu();
 });
+
+// Generate random movie and meal button
+var generateBtn = document.querySelector("#generate-button");
+// Generate with filters movie and meal button
+var filterBtn = document.querySelector("filter-button");
