3325384553724bfac2f745d25ca8cdacd6c00fd4
lilys0 <lsouryaphanh@gmail.com>
2021-09-28 21:59:08 +1000

    column 2 edit

--- assets/js/script.js
+++ assets/js/script.js
@@ -9,4 +9,11 @@ $(function () {
 // Generate random movie and meal button
 var generateBtn = document.querySelector("#generate-button");
 // Generate with filters movie and meal button
-var filterBtn = document.querySelector("filter-button");
+var filterBtn = document.querySelector("#filter-button");
+//Generated movie content
+var movieTitle = document.querySelector("movie-title");
+var overviewDiv = document.querySelector("overview");
+//Generated recipe content
+var recipeTitle = document.querySelector("recipe-title");
+var ingredientEl = document.querySelector("ingredients");
+var methodDiv = document.querySelector("method");
