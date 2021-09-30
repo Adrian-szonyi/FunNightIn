cb86f8e2a5beffc068e7fd886f8b37d0132b204a
lilys0 <lsouryaphanh@gmail.com>
2021-09-30 20:46:46 +1000

    style fix index

--- assets/js/readmore.js
+++ assets/js/readmore.js
@@ -1,15 +1,15 @@
-$(".readmore-link").click(function (e) {
-    // record if our text is expanded
-    var isExpanded = $(e.target).hasClass("expand");
-  
-    //close all open paragraphs
-    $(".readmore.expand").removeClass("expand");
-    $(".readmore-link.expand").removeClass("expand");
-  
-    // if target wasn't expand, then expand it
-    if (!isExpanded) {
-      $(e.target).parent(".readmore").addClass("expand");
-      $(e.target).addClass("expand");
-    }
-    
-  });
+// $(".readmore-link").click(function (e) {
+//     // record if our text is expanded
+//     var isExpanded = $(e.target).hasClass("expand");
+
+//     //close all open paragraphs
+//     $(".readmore.expand").removeClass("expand");
+//     $(".readmore-link.expand").removeClass("expand");
+
+//     // if target wasn't expand, then expand it
+//     if (!isExpanded) {
+//       $(e.target).parent(".readmore").addClass("expand");
+//       $(e.target).addClass("expand");
+//     }
+
+//   });
