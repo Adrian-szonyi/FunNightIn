1e9bc683987f0287dc25b58844ffa8279a7ef686
unknown <aszonyi49@gmail.com>
2021-10-02 20:04:59 +1000

    Set up the filter options to filter the API request for movies

--- assets/js/script.js
+++ assets/js/script.js
@@ -8,7 +8,7 @@ $(function () {
 
   $("#genre").selectmenu();
 
-  $("#rating").selectmenu();
+  $("#releaseperiod").selectmenu();
 });
 
 // Generate random movie and meal button
@@ -29,17 +29,61 @@ var filterBtn = document.querySelector("filter-button");
 var newImage = document.querySelector("#Newimage");
 var MovieImage = document.querySelector("#MoviePoster");
 
+var genre = document.querySelector("#genre");
+var releaseperiod = document.querySelector("#releaseperiod");
+var Comedy = document.querySelector("#Comedy")
+
+
+
 generateBtn.addEventListener("click", getMovies);
 
 async function getMovies() {
-  var randompage = Math.floor(Math.random() * 20);
-  var randommovie = Math.floor(Math.random() * 19);
+  var selectGenre = document.getElementById("genre").value
+    if (selectGenre === "Comedy") {
+      selectGenre = 35;
+    }
+    else if (selectGenre === "Action") {
+     selectGenre = 28;
+   }
+   else if (selectGenre === "Drama") {
+     selectGenre = 18;
+   }
+   else if (selectGenre === "Family") {
+     selectGenre = 10751;
+   }
+   else if (selectGenre === "Romance") {
+     selectGenre = 10749;
+   }
+   else {
+    selectGenre = 35;
+   }
+   var MaxReleaseDate = document.getElementById("releaseperiod").value
+   var MinReleaseDate = document.getElementById("releaseperiod").value
+   if (MaxReleaseDate === "2011 - 2021") {
+    MaxReleaseDate = "2021-09-09"
+    MinReleaseDate = "2011-01-01"
+   }
+   else if (MaxReleaseDate === "2000 - 2010") {
+    MaxReleaseDate = "2011-01-01"
+    MinReleaseDate = "2000-01-01"
+  }
+  else if (MaxReleaseDate === "1989 - 1999") {
+    MaxReleaseDate = "2000-01-01"
+    MinReleaseDate = "1989-01-01"
+  }
+ else {
+  MaxReleaseDate = "2021-09-09"
+  MinReleaseDate = "2011-01-01"
+ }
+  
+  var randompage = Math.floor((Math.random() * 1)+1);
+  var randommovie = Math.floor((Math.random() * 18)+1);
+  console.log(selectGenre)
   var movies = await fetch(
-    "https://api.themoviedb.org/3/discover/movie?sort_by=popularity.desc&vote_count.gte10000&original_language=en-US&page=" +
-      randompage +
-      "&api_key=7113d8f1b2a86dd4b4fe2e64488fe988"
+    "https://api.themoviedb.org/3/discover/movie?api_key=7113d8f1b2a86dd4b4fe2e64488fe988&sort_by=popularity.desc&release_date.gte=" + MinReleaseDate + "&release_date.lte=" + MaxReleaseDate + "&vote_count.gte=5000&with_genres=" + selectGenre + "&original_language=en-US&page=" +
+      randompage
+
   ).then((response) => response.json());
-  movies.results.length = 200;
   var movieposter =
     "https://image.tmdb.org/t/p/w500/" +
     movies.results[randommovie].poster_path;
@@ -59,3 +103,4 @@ async function getMovies() {
   movieTitle.textContent = movies.results[randommovie].original_title;
   overviewDiv.textContent = movies.results[randommovie].overview;
 }
+
